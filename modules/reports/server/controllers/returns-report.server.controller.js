a45c23565d5288b99ed8f97ff7e742a866249857
Jazy Llerena <jazy555@hotmail.com>
2016-03-22 18:52:19 -0400

    Create and Implement returns controller

---
+++ modules/reports/server/controllers/returns-report.server.controller.js
@@ -0,0 +1,24 @@
+'use strict';
+// variables for configuration
+var config = require('../../../../config/env/local'),
+		db = requrie('./reports.server.controller.js');
+
+// variables to set up mws client
+var MWS = require('mws-sdk'),
+    client = new MWS.Client(config.accessKeyId, config.secretAccessKey, config.merchantId, {}),
+    marketPlaceId = 'ATVPDKIKX0DER';
+
+/*
+ *	This function will take a returns jReport obj and will do the neccessary tasks to insert it into the 
+ *	database
+ *	The return will be the same report object on success. On fail the callback will be given an object with
+ *	an 'Error' key and an error message value.
+ */
+var ProcessReturnsReport = exports.ProcessReturnsReport = function(reportObj, callback) {
+	//Just pass it to the database controller (reports.server.controller.js)
+	//NOTE: Maybe this can make sure that the type is present and correct?
+	db.create(reportObj, callback);
+	//gg
+}
+
+
