167a32f6f81f37635f7608e751cbc409039eb173
Jazy Llerena <jazy555@hotmail.com>
2016-03-28 22:03:21 -0400

    Add test to add and remove documents from database

--- modules/reports/tests/server/orders-report.server.controller.tests.js
+++ modules/reports/tests/server/orders-report.server.controller.tests.js
@@ -6,8 +6,13 @@
 var should = require('should'),
   mongoose = require('mongoose'),
 	fs = require('fs'),
+	db = require('../../server/controllers/reports.server.controller.js'),
+	config = require('../../../../config/env/development'),
 	reports = require('../../server/controllers/mws-reports.server.controller.js'),
 	orders = require('../../server/controllers/orders-report.server.controller.js');
+
+//Connect to the mongoose db
+mongoose.connect(config.db.uri);
 
 /**
  * Globals
@@ -52,14 +57,48 @@ describe('Report Controller Unit Tests:', function () {
 			this.timeout(60000);
 			orders.ProcessOrdersReport(jReport, function(result) {
 				//result....
-				console.log(JSON.stringify(result.ReportRows, null, 2));
-				for (var i = 0; i < result.ReportRows.length; i++){
-					result.ReportRows[i].should.have.property('fees');
-					result.ReportRows[i].fees.should.not.be.type('undefined');
+				//console.log(JSON.stringify(result.ReportRows, null, 2));
+				var r = result.Report;
+				for (var i = 0; i < r.ReportRows.length; i++){
+					r.ReportRows[i].should.have.property('fees');
+					r.ReportRows[i].fees.should.not.be.type('undefined');
 				}
+				result.should.have.property('Docs');
+				result.Docs.should.have.property('insertedIds');
+				console.log(result.Docs.insertedIds.length + ' documents uploaded');
+				jReport = result;
 				done();
 			});
     });
+
+		it('should have been added to database', function (done) {
+			this.timeout(60000);
+			db.list(jReport.Report.ReportType, function(result) {
+				//console.log(JSON.stringify(result, null, 2));
+				result.should.not.have.property('Error');
+				console.log(result.Docs.length + ' documents in collection');
+				//console.log(JSON.stringify(result, null, 2));
+				var ids = jReport.Docs.insertedIds;
+				for (var i=0; i<ids.length; i++) {
+					//console.log(ids[i]);
+					//result.Docs.should.containDeep(ids[i]); 
+					//Dammit cant get this shit to work
+				}
+				done();
+			});
+		});
+
+		it('should remove documents from the database', function (done) {
+			db.delete(jReport.Report.ReportType, {
+				_id: {
+					$in: jReport.Docs.insertedIds
+				}
+			}, function(result) {
+				result.should.not.have.property('Error');
+				console.log('Deleted ' + jReport.Docs.insertedIds.length + ' documents');
+				done();
+			});
+		});
 		/*
     it('should be able to show an error when try to save without title', function (done) {
       report.title = '';
